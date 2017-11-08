f3781ee0f16e0f5d8a1dd0500c12417c92187d78
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-11-08 14:49:31 +0800

    Initial commit

---
+++ app/src/main/java/com/alizhezi/recycler/refresh/demo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.alizhezi.recycler.refresh.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void LoadMore(View view){
+
+        startActivity(new Intent(MainActivity.this,LoadMoreActivity.class));
+    }
+
+    public void LoadMoreWrapper(View view){
+
+        startActivity(new Intent(MainActivity.this,LoadMoreWrapperActivity.class));
+    }
+}
